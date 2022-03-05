fb89afff3f366679ce1b12853b772daf4a5a39a1
蔡升豪 <93434826+sh-tasi@users.noreply.github.com>
2022-03-05 13:42:10 +0800

    ver1.1.4

--- api/attractions_sys/view.py
+++ api/attractions_sys/view.py
@@ -4,26 +4,16 @@ import json,math
 import mysql.connector
 attractions_sys = Blueprint('attractions_sys',__name__,)
 def search_key(page):
-    if page=="0":
-        sql="SELECT `id`,`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`,`images` FROM `TAIPEI_VIEW`WHERE `name` like %s LIMIT 12"
-        print(sql)
-        return(sql)
-    elif page=="1":
-        sql="SELECT `id`,`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`,`images` FROM `TAIPEI_VIEW`WHERE `name` like %s LIMIT 12 OFFSET 12"
-        print(sql)
-        return(sql)
-    elif page=="2":
-        sql="SELECT `id`,`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`,`images` FROM `TAIPEI_VIEW`WHERE `name` like %s LIMIT 12 OFFSET 24"
-        print(sql)
-    elif page=="3":
-        sql="SELECT `id`,`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`,`images` FROM `TAIPEI_VIEW`WHERE `name` like %s LIMIT 12 OFFSET 36"
-        print(sql)
-    elif page=="4":
-        sql="SELECT `id`,`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`,`images` FROM `TAIPEI_VIEW`WHERE `name` like %s LIMIT 12 OFFSET 48"
-        print(sql)
-    else:
-        sql="SELECT `id`,`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`,`images` FROM `TAIPEI_VIEW`WHERE `name` like %s LIMIT 12 OFFSET 48"
-        print(sql)
+    sql_select_key="SELECT `id`,`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`,`images` FROM `TAIPEI_VIEW`WHERE `name` like %s LIMIT "
+    sql_page=page*12
+    sql=sql_select_key+" "+str(sql_page)+","+"12"
+    return(sql)
+def view_select(page):
+    sql_select="SELECT  `id`,`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`,`images` FROM  `TAIPEI_VIEW` LIMIT "
+    sql_page=12*page
+    sql=sql_select+" "+str(sql_page)+","+"12"
+    print(sql)
+    return(sql)
 @attractions_sys.route('/attractions',methods=["GET"])
 def attractions_search():
     page=request.args.get("page","0")
@@ -38,9 +28,34 @@ def attractions_search():
     )
     mycursor = mydb.cursor()
     if keyword=="*":
-        # sql="SELECT count(*)FROM `TAIPEI_VIEW`"
-        # mycursor.execute(sql)
-        print(keyword)
+        view_page=[]
+        sql_conut="SELECT count(*)FROM `TAIPEI_VIEW`"
+        mycursor.execute(sql_conut)
+        view_count=mycursor.fetchall()
+        view_count=view_count[0][0]           #所有資料總數
+        view_maxpage=view_count/12            #12筆為一頁資料量
+        view_maxpage=math.ceil(view_maxpage)  #最大頁數
+        page_int=int(page)                    #PAGE轉換成數字做判斷使用
+        sql=view_select(page_int)             #取出第N頁資料
+        mycursor.execute(sql)              
+        datas_field=list(zip(*mycursor.description))[0]
+        myresult_page=mycursor.fetchall()
+        for i in range(len(myresult_page)):   #SQL資料整理
+            data_resoponse=dict(zip(datas_field,myresult_page[i]))
+            data_resoponse["images"]=[data_resoponse["images"]]
+            view_page.append(data_resoponse)
+        view_maxpage=view_maxpage-1 #第0頁開始計算
+        if page_int<view_maxpage:    
+            view_page_js=jsonify({"nextPage":int(page)+1,"data":view_page})
+            mycursor.close()
+            mydb.close()
+            return(view_page_js)
+        else :
+            view_page_js=jsonify({"nextPage":None,"data":view_page})
+            mycursor.close()
+            mydb.close()
+            return(view_page_js)
+        '''    
         if page=="0":
             view_page=[]
             sql="SELECT `id`,`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`,`images` FROM `TAIPEI_VIEW`LIMIT 12" 
@@ -130,6 +145,7 @@ def attractions_search():
             mycursor.close()
             mydb.close()
             return(view_page_js)
+        '''
     else :
         sql_count="SELECT count(*) FROM `TAIPEI_VIEW` WHERE `name` like %s  "
         keyword_sql=["%"+keyword+"%"]
@@ -137,7 +153,8 @@ def attractions_search():
         myresult_page_count=mycursor.fetchall()
         page_max=myresult_page_count[0][0]/12   
         page_max=math.ceil(page_max)-1
-        if page_max==int(page):
+        page_int=int(page)
+        if page_int<page_max:
         # sql="SELECT `id` FROM `TAIPEI_VIEW` WHERE `name` like %s LIMIT 12 OFFSET 2 "
             sql=search_key(page)
             keyword_sql=["%"+keyword+"%"]
@@ -150,37 +167,28 @@ def attractions_search():
                 data_resoponse=dict(zip(datas_field,myresult_key[i]))
                 view_key_data.append(data_resoponse)
                 print(data_resoponse)
-            view_page_js=jsonify({"nextPage":"此頁為最終頁","data":view_key_data})
+            view_page_js=jsonify({"nextPage":page_int+1,"data":view_key_data})
             print(myresult_key)
             mycursor.close()
             mydb.close()
             return(view_page_js)
-        elif int(page) > page_max:
-        # print(view_key_data)
-        # print(datas_field)
-        # print(view_page_js)
-        # if myresult_key==[]:
-        # sql_count="SELECT count(*) FROM `TAIPEI_VIEW` WHERE `name` like %s  "
-        # mycursor.execute(sql_count,keyword_sql)
-        # myresult_page_count=mycursor.fetchall()
-        # page_max=myresult_page_count[0][0]/12
-        # page_max=math.ceil(page_max)-1
-            response_body={
-                "error":"true",
-                "message":"最後一頁為第"+str(page_max)+"頁"
-            }
-            response_body=jsonify(response_body)
+        else :
+            sql=search_key(page)
+            keyword_sql=["%"+keyword+"%"]
+            print(keyword_sql)
+            mycursor.execute(sql,keyword_sql)
+            myresult_key=mycursor.fetchall()
+            view_key_data=[]
+            datas_field=list(zip(*mycursor.description))[0]
+            for i in range(len(myresult_key)):
+                data_resoponse=dict(zip(datas_field,myresult_key[i]))
+                view_key_data.append(data_resoponse)
+                print(data_resoponse)
+            view_page_js=jsonify({"nextPage":None,"data":view_key_data})
+            print(myresult_key)
             mycursor.close()
             mydb.close()
-            return(response_body)  
-    response_body={
-        "error":"true",
-        "message":"最後一頁為第4頁"
-    }
-    response_body=jsonify(response_body)
-    mycursor.close()
-    mydb.close()
-    return(response_body)
+            return(view_page_js) 
 @attractions_sys.route('/attraction/<attractionID>')
 def attraction_id_search(attractionID):
     mydb = mysql.connector.connect(
@@ -192,7 +200,7 @@ def attraction_id_search(attractionID):
     )
     mycursor = mydb.cursor()
     attractionID_sql=(attractionID,)
-    sql=sql="SELECT `id`,`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`,`images` FROM `TAIPEI_VIEW` WHERE `view_id`=%s"
+    sql="SELECT `id`,`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`,`images` FROM `TAIPEI_VIEW` WHERE `view_id`=%s"
     mycursor.execute(sql,attractionID_sql)
     myresult=mycursor.fetchone()
     datas_field=list(zip(*mycursor.description))[0]
@@ -205,11 +213,19 @@ def attraction_id_search(attractionID):
         mydb.close()
         return(data_resoponse_js)
     else:
+        sql_count="SELECT count(*) FROM `TAIPEI_VIEW`"
+        mycursor.execute(sql_count)
+        myresult=mycursor.fetchone()
+        print(myresult)
+        view_count=myresult[0]
+        print(view_count)
         response_body={
         "error":"true",
-        "message":"景點id錯誤，請輸入1~58"
+        "message":"景點id超出範圍,目前共有"+str(view_count)+"個資料"
         }
         response_body=jsonify(response_body)
+        mycursor.close()
+        mydb.close()
         return(response_body)
 @attractions_sys.route('/attraction/')
 def attraction_id_error():
